d090fbb2ac4e8a62c6fd25c84558d2c1a161e249
volaya <volayaf@gmail.com>
2017-09-04 09:47:58 +0200

    added missing file

---
+++ datacubeplugin/selectionmaptools.py
@@ -0,0 +1,80 @@
+from qgis.PyQt.QtCore import Qt
+from qgis.PyQt.QtWidgets import QApplication
+from qgis.PyQt.QtGui import QCursor
+
+from qgis.core import QgsCoordinateReferenceSystem, QgsCoordinateTransform, QGis, QgsPoint, QgsRectangle
+from qgis.gui import QgsMapTool, QgsMessageBar, QgsMapToolEmitPoint, QgsRubberBand
+from qgis.utils import iface
+
+from datacubeplugin.gui.plotwidget import plotWidget
+
+class PointSelectionMapTool(QgsMapTool):
+
+    def __init__(self, canvas):
+        QgsMapTool.__init__(self, canvas)
+        self.setCursor(Qt.CrossCursor)
+
+    def canvasReleaseEvent(self, e):
+        plotWidget.show()
+        pt = self.toMapCoordinates(e.pos())
+        plotWidget.setPoint(pt)
+
+class RegionSelectionMapTool(QgsMapTool):
+
+    def __init__(self, canvas):
+        self.canvas = canvas
+        QgsMapTool.__init__(self, self.canvas)
+        self.setCursor(Qt.CrossCursor)
+        self.rubberBand = QgsRubberBand(self.canvas, QGis.Polygon)
+        self.rubberBand.setColor(Qt.red)
+        self.rubberBand.setWidth(1)
+        self.reset()
+
+    def reset(self):
+        self.startPoint = self.endPoint = None
+        self.isEmittingPoint = False
+        self.rubberBand.reset(QGis.Polygon)
+
+    def canvasPressEvent(self, e):
+        self.startPoint = self.toMapCoordinates(e.pos())
+        self.endPoint = self.startPoint
+        self.isEmittingPoint = True
+        self.showRect(self.startPoint, self.endPoint)
+
+    def canvasReleaseEvent(self, e):
+        self.isEmittingPoint = False
+        plotWidget.setRectangle(self.rectangle())
+        self.reset()
+
+
+    def canvasMoveEvent(self, e):
+        if not self.isEmittingPoint:
+            return
+
+        self.endPoint = self.toMapCoordinates(e.pos())
+        self.showRect(self.startPoint, self.endPoint)
+
+    def showRect(self, startPoint, endPoint):
+        self.rubberBand.reset(QGis.Polygon)
+        if startPoint.x() == endPoint.x() or startPoint.y() == endPoint.y():
+            return
+
+        point1 = QgsPoint(startPoint.x(), startPoint.y())
+        point2 = QgsPoint(startPoint.x(), endPoint.y())
+        point3 = QgsPoint(endPoint.x(), endPoint.y())
+        point4 = QgsPoint(endPoint.x(), startPoint.y())
+
+        self.rubberBand.addPoint(point1, False)
+        self.rubberBand.addPoint(point2, False)
+        self.rubberBand.addPoint(point3, False)
+        self.rubberBand.addPoint(point4, True)    # true to update canvas
+        self.rubberBand.show()
+
+    def rectangle(self):
+        if self.startPoint is None or self.endPoint is None:
+            return None
+        elif self.startPoint.x() == self.endPoint.x() or self.startPoint.y() == self.endPoint.y():
+            return None
+
+        return QgsRectangle(self.startPoint, self.endPoint)
+
