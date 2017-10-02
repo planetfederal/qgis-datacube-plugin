bac21107273d8b2fad9cee1cadb0e6deacb13bc0
volaya <volayaf@gmail.com>
2017-10-02 12:45:44 +0200

    added test

--- datacubeplugin/tests/testerplugin.py
+++ datacubeplugin/tests/testerplugin.py
@@ -3,14 +3,19 @@
 
 import os
 import unittest
-
+from qgis.PyQt.QtGui import QApplication
 try:
     from qgistester.test import Test
     from qgistester.utils import layerFromName
 except:
     pass
 
-
+def copyDatacubeFolder():
+    path = os.path.join(os.path.dirname(__file__), "data", "datacube")
+    cb = QApplication.clipboard()
+    cb.clear(mode=cb.Clipboard )
+    cb.setText(path, mode=cb.Clipboard)
+    
 def functionalTests():
     try:
         from qgistester.test import Test
@@ -21,10 +26,22 @@ def functionalTests():
     def sampleMethod(self):
         pass
 
-    sampleTest = Test("Sample test")
-    sampleTest.addStep("Sample step", sampleMethod)
+    plotTest = Test("Plot workflow")
+    plotTest.addStep("Copy folder path", copyDatacubeFolder)
+    plotTest.addStep("Add data source to plugin main panel. Datacube URL has been copied to the clipboard")
+    plotTest.addStep("Add all layers from the ls7 coverage")
+    plotTest.addStep("Move to the 'Plot' tab and click the 'Select point tool' button." 
+                     "Click on a point in the canvas. You should see a plot in the plot panel.", isVerifyStep=True)
+    plotTest.addStep("Click on the 'Select region tool'. Click and drag a small rectangle in the canvas."
+                     " You should see a box and whiskers plot in the plot panel", isVerifyStep=True)
+    plotTest.addStep("Move the sliders in the 'Plot' tab and verify that the plot panel reacts correctly", isVerifyStep=True)
+    plotTest.addStep("Change the parameter to plot and verify that the plot panel reacts correctly", isVerifyStep=True)
+    plotTest.addStep("Change the coverage to plot and verify that the plot panel is cleared", isVerifyStep=True)
+    plotTest.addStep("Change back the coverage to plot and verify that the plot panel shows a plot again", isVerifyStep=True)
 
-    return [sampleTest]
+
+
+    return [plotTest]
 
 
 class DataCubePluginTest(unittest.TestCase):
